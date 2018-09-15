30480682d6893ab0ed0fd4ac9d4e83d039447f7f
Steven Xu <siyuanx@usc.edu>
2018-09-15 14:07:43 -0700

    refactored project

---
+++ src/Map/FlightMap.java
@@ -0,0 +1,8 @@
+package Map;
+
+public class FlightMap {
+    public static void printMSG()
+    {
+        System.out.println("Hello from Map.FlightMap!");
+    }
+}
--- src/Map/SearchMap.java
+++ src/Map/SearchMap.java
@@ -1,13 +1,17 @@
+package Map;
+
+import Map.*;
 public class SearchMap {
     /*
      * @param: arguments
      */
     public static void main(String[] args)
     {
+        FlightMap.printMSG();
         // checks for input errors
         if(args.length >= 3 || args.length < 2)
         {
-            System.out.println("Input format should be: \"java SearchMap input_file output_file\"");
+            System.out.println("Input format should be: \"java Map.SearchMap input_file output_file\"");
         }
     }
 }
